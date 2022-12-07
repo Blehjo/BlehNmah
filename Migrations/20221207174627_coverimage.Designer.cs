f94577080cad0936f98654f472df335fb229ab94
bseton11-YJKnqQ <bseton11@gmail.com>
2022-12-07 11:59:47 -0600

    updated styling on collections index page

--- Migrations/20221207174627_coverimage.Designer.cs
+++ Migrations/20221207174627_coverimage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesBlehNmah.Data;
 
@@ -10,9 +11,11 @@ using RazorPagesBlehNmah.Data;
 namespace RazorPagesBlehNmah.Migrations
 {
     [DbContext(typeof(RazorPagesGalleryContext))]
-    partial class RazorPagesGalleryContextModelSnapshot : ModelSnapshot
+    [Migration("20221207174627_coverimage")]
+    partial class coverimage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
---
+++ Migrations/20221207174627_coverimage.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesBlehNmah.Migrations
+{
+    /// <inheritdoc />
+    public partial class coverimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Photograph",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Painting",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImage",
+                table: "Collection",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Collection",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Photograph");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Painting");
+
+            migrationBuilder.DropColumn(
+                name: "CoverImage",
+                table: "Collection");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Collection");
+        }
+    }
+}
