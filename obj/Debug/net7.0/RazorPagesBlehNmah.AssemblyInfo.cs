94bf32439878487fca87a8212c88b7069eaad3cd
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-05 00:02:04 -0600

    updated collections

--- obj/Debug/net7.0/RazorPagesBlehNmah.AssemblyInfo.cs
+++ obj/Debug/net7.0/RazorPagesBlehNmah.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("868e9a68-4f3b-4124-a9ec-d857c70190e5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesBlehNmah")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
