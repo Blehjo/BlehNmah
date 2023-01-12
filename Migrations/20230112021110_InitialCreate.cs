90cb408d6584e26e32b8eee2d4d4269c68d4a52a
Blehjo <blehjooo@gmail.com>
2023-01-11 20:11:31 -0600

    added migrations

---
+++ Migrations/20230112021110_InitialCreate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesBlehNmah.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collection",
+                columns: table => new
+                {
+                    CollectionID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CollectionName = table.Column<string>(type: "TEXT", nullable: true),
+                    CoverImage = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collection", x => x.CollectionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Painting",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CollectionID = table.Column<int>(type: "INTEGER", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Medium = table.Column<string>(type: "TEXT", nullable: true),
+                    Location = table.Column<string>(type: "TEXT", nullable: true),
+                    MediaLocation = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Painting", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Painting_Collection_CollectionID",
+                        column: x => x.CollectionID,
+                        principalTable: "Collection",
+                        principalColumn: "CollectionID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photograph",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CollectionID = table.Column<int>(type: "INTEGER", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Location = table.Column<string>(type: "TEXT", nullable: true),
+                    MediaLocation = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photograph", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photograph_Collection_CollectionID",
+                        column: x => x.CollectionID,
+                        principalTable: "Collection",
+                        principalColumn: "CollectionID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Painting_CollectionID",
+                table: "Painting",
+                column: "CollectionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photograph_CollectionID",
+                table: "Photograph",
+                column: "CollectionID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Painting");
+
+            migrationBuilder.DropTable(
+                name: "Photograph");
+
+            migrationBuilder.DropTable(
+                name: "Collection");
+        }
+    }
+}
